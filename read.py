c24dcda42c44b96075322772adcc97cace94321d
carloarm <carloarm@gmail.com>
2018-03-15 10:49:20 -0300

    [B0.1] Beta de proyecto. Contiene: Archivo de configuración. File search (sólo desde linux). Versión genera archivos de proveedores, versión con reembolsos para B0.2

--- read.py
+++ read.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
 """
 Actualmente genera un archivo sample.xlsx con el formato de las nominas de Office Banking.
 Todo:
   Hacer más filtros.
-
   Filtrar conmutativamente.
-
   Completar información de nominas
 """
 from __future__ import print_function
@@ -13,134 +10,90 @@ from xlrd.sheet import ctype_text
 import xlrd
 import write
 from datetime import date, time
+from datetime import datetime  
+from datetime import timedelta 
 
 class Reader:
-  """Clase lectora de archivos Excel"""
-  def __init__(self, fileName, index = 0):
-    """Intancia un objeto de clase Reader.
+    """Clase lectora de archivos Excel"""
 
+    def __init__(self, fileName, index=0):
+        """Intancia un objeto de clase Reader.
     Args:
       filename (string) : Full Filepath del archivo
       index (int)       : indice de la hoja a leer (default = 0)"""
-    self.fileName = fileName;
-    self.xlrdWorkbook = xlrd.open_workbook(fileName)
-    self.xlrdSheet = self.xlrdWorkbook.sheet_by_index(index)
+        self.fileName = fileName
+        self.xlrdWorkbook = xlrd.open_workbook(fileName)
+        self.xlrdSheet = self.xlrdWorkbook.sheet_by_index(index)
 
-  def setSheetByIndex(self, index):
-    self.xlrdSheet = self.xlrdWorkbook.sheet_by_index(index)
+    def setSheetByIndex(self, index):
+        self.xlrdSheet = self.xlrdWorkbook.sheet_by_index(index)
 
-  def setSheetByName(self, name):
-    self.xlrdSheet = self.xlrdWorkbook.sheet_by_name(name)
+    def setSheetByName(self, name):
+        self.xlrdSheet = self.xlrdWorkbook.sheet_by_name(name)
 
-  def getRowList(self):
-    """Genera una lista de filas.
-
+    def getRowList(self):
+        """Genera una lista de filas.
     Returns:
       object[][] : lista de filas (cada fila es una lista de objetos)"""
-    ret = [];
-    for rowInd in range(0, self.xlrdSheet.nrows):
-      ret.append(self.xlrdSheet.row(rowInd))
-    return ret
+        ret = []
+        for rowInd in range(0, self.xlrdSheet.nrows):
+            ret.append(self.xlrdSheet.row(rowInd))
+        return ret
 
-  def filterDate(self, rowList, dt, columnIndex):
-    """Retorna una lista de filas cuyas fechas de pago coinciden con el argumento.
-
+    def filterDate(self, rowList, dt, columnIndex):
+        """Retorna una lista de filas cuyas fechas de pago coinciden con el argumento.
     Args:
       rowList (obj[][])       : Lista de filas
       dt (datetime.datetime)  : Instancia de datetime.datetime
       columnIndex (int)       : Indice de la columna que contiene la fecha
-
     Returns:
       object[][] : Lista de filas (cada fila es una fila de objetos)"""
-    ret = []
-    for row in rowList:
-      try:
-        py_date = toDatetime(row[columnIndex], self.xlrdWorkbook)
-      except ValueError: #greedy: empty row
-        continue;
-      if dt == py_date:
-        ret.append(row);     
-    return ret
+        ret = []
+        for row in rowList:
+            try:
+                py_date = toDatetime(row[columnIndex], self.xlrdWorkbook)
+            except ValueError:  #greedy: empty row
+                continue
+            if (dt == py_date) | (dt - timedelta(days=7) < py_date):
+                ret.append(row)
+        return ret
 
-  def filterType(self, rowList, orderType, columnIndex):
-    """Retorna una lista de filas cuyos tipos de orden (OP, TDV, etc) coinciden
+    def filterType(self, rowList, orderType, columnIndex):
+        """Retorna una lista de filas cuyos tipos de orden (OP, TDV, etc) coinciden
     con el argumento.
-
     Args:
       rowList (obj[][])       : Lista de filas
       orderType (string)      : Tipo de orden de compra
       columnIndex (int)       : Indice de la columna que contiene el tipo"""
-    ret = []
-    for row in rowList:
-      if orderType in row[columnIndex].value:
-        ret.append(row);
-    return ret
+        ret = []
+        for row in rowList:
+            if orderType in row[columnIndex].value:
+                ret.append(row)
+        return ret
+    
+    def filterReemb(self, rowList):
+        ret = []
+        for row in rowList:
+            try:
+              if ('X' in row[0].value) | ('x' in row[0].value):
+                  ret.append(row)
+            except Exception:
+              continue
+        return ret
 
-  def printSheet(self):
-    """Imprime fila por fila la hoja activa.
-
+    def printSheet(self):
+        """Imprime fila por fila la hoja activa.
     Args:
       sheetIndex (int) : indice cero-indexado de la hoja"""
-    print ('Sheet name: %s' % self.xlrdSheet.name)
-    num_cols = self.xlrdSheet.ncols
-    for rowInd in range(0, self.xlrdSheet.nrows):
-      print ('-'*40)
-      print ('Row: %s' % rowInd)
-      for colInd in range(0, num_cols):  # Iterate through columns
-        cell_obj = self.xlrdSheet.cell(rowInd, colInd)
-        print ('Column: [%s] cell_obj: [%s]' % (colInd, cell_obj))
+        print('Sheet name: %s' % self.xlrdSheet.name)
+        num_cols = self.xlrdSheet.ncols
+        for rowInd in range(0, self.xlrdSheet.nrows):
+            print('-' * 40)
+            print('Row: %s' % rowInd)
+            for colInd in range(0, num_cols):  # Iterate through columns
+                cell_obj = self.xlrdSheet.cell(rowInd, colInd)
+                print('Column: [%s] cell_obj: [%s]' % (colInd, cell_obj))
+
 
 def toDatetime(xldateCell, book):
-  return xlrd.xldate.xldate_as_datetime(xldateCell.value, book.datemode)
-
-'''
-Legacy
-
-print('(Column #) type:value')
-for idx, cell_obj in enumerate(row):
-cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')
-print('(%s) %s %s' % (idx, cell_type_str, cell_obj.value))
-
-def filterDate(filen, dt):
-  """Retorna un arreglo de filas cuyas fechas de pago coinciden con el argumento.
-
-  Args:
-    dt (datetime.datetime) : Instancia de datetime.datetime
-
-  Returns:
-    object[][] : Arreglo de filas (cada fila es un arreglo de objetos)"""
-  ret = []
-  fname = filen # join(dirname(dirname(abspath(__file__))), 'Flujo caja 2018.xlsx') 
-  xlrdWorkbook = xlrd.open_workbook(fname)
-  xlrdSheet = xlrdWorkbook.sheet_by_name('Proveedores') 
-  for rowInd in range(0, xlrdSheet.nrows):    # Iterate through rows
-    a = xlrdSheet.row(rowInd)
-    # print ('%s' % a)
-    try:      
-      py_date = toDatetime(a[5], xlrdWorkbook)
-    except ValueError: #greedy: empty row
-      continue
-    if dt == py_date:
-      ret.append(a);
-      # print ('%s, %s, %s' % (a[0], a[1], py_date))
-  return ret
-
-def printSheet(sheetIndex):
-  """Imprime fila por fila la hoja pedida del libro
-  Args:
-    sheetIndex (int) : indice cero-indexado de la hoja"""
-  fname = join(dirname(abspath(__file__)), 'Flujo caja 2018.xlsx')
-  xlrdWorkbook = xlrd.open_workbook(fname)
-  #sheet_names = xlrdWorkbook.sheet_names()
-  #print('Sheet Names', sheet_names)
-  xlrdSheet = xlrdWorkbook.sheet_by_index(sheetIndex)
-  print ('Sheet name: %s' % xlrdSheet.name)
-  num_cols = xlrdSheet.ncols
-  for rowInd in range(0, xlrdSheet.nrows):
-    print ('-'*40)
-    print ('Row: %s' % rowInd)
-    for colInd in range(0, num_cols):  # Iterate through columns
-      cell_obj = xlrdSheet.cell(rowInd, colInd)
-      print ('Column: [%s] cell_obj: [%s]' % (colInd, cell_obj))
-
-    '''
+    return xlrd.xldate.xldate_as_datetime(int(xldateCell.value), book.datemode)
--- write.py
+++ write.py
@@ -6,8 +6,8 @@ from openpyxl.styles import Font, Color, PatternFill, Border, Side
 from os.path import join, dirname, abspath
 
 class Writer:
-  def __init__(self, filename, writeList = [], writeFormat = []):
-    """Instancia un objeto de clase Writer.
+    def __init__(self, writeList=[], writeFormat=[]):
+        """Instancia un objeto de clase Writer.
 
     Args:
       filename (string)       : Fullpath del archivo
@@ -16,91 +16,79 @@ class Writer:
                                 indica que el elemento en posición Y de la lista writeList
                                 debe ir en la columna X (0-indexada) de la nomina de pago.
                                 Ciertos valores deben ser djeados en Nene (Cod Banco), Cuenta"""
-    self.filename = filename
-    self.format = writeFormat
-    self.list = writeList
+        
+        '''Formato OB: RUT, Nombre, Modalidad, Código Banco, Cuenta, NFactura, Monto'''
+        #self.FORMAT_PROVEEDOR = [0, 1, '3', 16, 17, 2, 3]
+        self.FORMAT_PROVEEDOR = [0, 1, '3', 16, 17, 2, 3]
+        self.FORMAT_REEMBOLSO = [0, 1, '3', 16, 17, 2, 3]
 
-  def setWriteList(self, list):
-    self.list = list
+    def write_provider(self,
+                       filteredList,
+                       outputFileName,
+                       dbFileRows=[None] * 7):
+        wb = Workbook()
+        ws = wb.active
+        self.writeHeader(ws)
+        joinedList = self.join_listsByRut(dbFileRows, filteredList)
+        ws = self.get_file_content(ws, joinedList, self.FORMAT_PROVEEDOR)
+        wb.save(outputFileName)
 
-  def setFormatList(self, list):
-    self.format = list
+    def write_reembolso(self,
+                        filteredList,
+                        outputFileName,
+                        dbFileRows=[None] * 7):
+        wb = Workbook()
+        ws = wb.active
+        self.writeHeader(ws)
+        joinedList = self.join_listsByRut(dbFileRows, filteredList)
+        ws = self.get_file_content(ws, joinedList, self.FORMAT_REEMBOLSO)
+        wb.save(outputFileName)
 
-  def write(self, dbFileRows = [None]*7):
-    """Escribe en la nomina los datos de la lista."""
-    if not self.list:
-      print ("No ha definido una lista para escribir")
-      return
-    if not self.format:
-      print ("No ha definido una lista de formato")
-      return
-    wb = Workbook()
-    ws = wb.active
+    def join_listsByRut(self, dbFileRows, filteredList):
+        ret = []
+        for row in filteredList:
+            try:
+                for rowProveed in dbFileRows:
+                    if str(row[0].value) in str(rowProveed[0].value):
+                        ret.append(row + rowProveed)
+                        #ret.append(row)
+                        break
+            except Exception:
+                continue
+        return ret
+    
+    def get_file_content(self, outputFileContent, joinedList, format):
+        newRow = [None] * 29
+        for row in joinedList:
+            for rowInd in range(0, len(format)):
+                if(isinstance(format[rowInd], str)):
+                    newRow[rowInd] = format[rowInd]
+                else:
+                    newRow[rowInd] = row[format[rowInd]].value
+            newRow[28] = row[3].value
+            outputFileContent.append(newRow)
+        return outputFileContent
 
-    # Column Header
-    self.writeHeader(ws);
-
-    #TODO: FORMAT LOGIC GOES HERE    
-    for row in self.list:
-      data = [None] * 7
-      for i in range(len(self.format)):
-        try:
-          data[i] = row[self.format[i]];
-        except TypeError: #Greedy, None index
-          continue
-      data = self.completeRow(data, dbFileRows)
-      if data[4]:
-        ws.append(data)
-
-    wb.save(self.filename);
-
-  def writeHeader(self, ws):
-    ws.append(['Rut Beneficiario', 'Nombre Beneficiario', 'Cod. Modalidad', 'Cod Banco',
-      'Cta Abono', 'N Factura 1', 'Monto 1', 'N Factura 2', 'Monto 2', 'N Factura 3',
-      'Monto 3', 'N Factura 3', 'Monto 4', 'N Factura 4', 'Monto 5', 'N Factura 5',
-      'Monto 6', 'N Factura 6', 'Monto 7', 'N Factura 7', 'Monto 8', 'N Factura 8',
-      'Monto 9', 'N Factura 9', 'Monto 10', 'N Factura 10', 'Monto 11', 'N Factura 11',
-      'Monto Total']) 
-    sd = Side(border_style = 'thin', color = 'FF000000')
-    for cell in ws["1:1"]:
-      cell.font = Font(name='Arial', size = 9, color = 'FF0000FF')
-      cell.fill = PatternFill(fill_type = 'solid', start_color = "ffcccccc")
-      cell.border = Border(left = sd, right = sd, top = sd, bottom= sd)
-    ws.column_dimensions["A"].width = 15
-    ws.column_dimensions["B"].width = 40
-    ws.column_dimensions["C"].width = 15
-    ws.column_dimensions["D"].width = 15
-    ws.column_dimensions["E"].width = 15
-    ws.column_dimensions["G"].width = 15
-    ws.column_dimensions["F"].width = 15
-    ws.column_dimensions["AC"].width = 15
-
-  def completeRow(self, data, dbFileRows):
-    '''data[0] : RUT
-    data[1] : Nombre
-    data[2] : Codigo modalidad ---
-    data[3] : Cod Banco --
-    data[4] : Cuenta Abono --
-    data[5] : Factura
-    data[6] : Monto'''
-    ret = [None]*29
-    ret[0] = data[0].value
-    ret[1] = data[1].value
-    ret[2] = 3
-    for row in dbFileRows:
-      try:
-        if row[0].value[:-1] == data[0].value[:-1]:
-          ret[3] = row[3].value
-          try:
-            ret[4] = int(row[4].value)
-          except:
-            ret[4] = row[4].value
-      except:
-        continue
-    ret[5] = data[5].value
-    ret[6] = data[6].value
-    ret[28] = ret[6]
-    print '-',
-    return ret
-  
-
+    def writeHeader(self, ws):
+        ws.append([
+            'Rut Beneficiario', 'Nombre Beneficiario', 'Cod. Modalidad',
+            'Cod Banco', 'Cta Abono', 'N Factura 1', 'Monto 1', 'N Factura 2',
+            'Monto 2', 'N Factura 3', 'Monto 3', 'N Factura 3', 'Monto 4',
+            'N Factura 4', 'Monto 5', 'N Factura 5', 'Monto 6', 'N Factura 6',
+            'Monto 7', 'N Factura 7', 'Monto 8', 'N Factura 8', 'Monto 9',
+            'N Factura 9', 'Monto 10', 'N Factura 10', 'Monto 11',
+            'N Factura 11', 'Monto Total'
+        ])
+        sd = Side(border_style='thin', color='FF000000')
+        for cell in ws["1:1"]:
+            cell.font = Font(name='Arial', size=9, color='FF0000FF')
+            cell.fill = PatternFill(fill_type='solid', start_color="ffcccccc")
+            cell.border = Border(left=sd, right=sd, top=sd, bottom=sd)
+        ws.column_dimensions["A"].width = 15
+        ws.column_dimensions["B"].width = 40
+        ws.column_dimensions["C"].width = 15
+        ws.column_dimensions["D"].width = 15
+        ws.column_dimensions["E"].width = 15
+        ws.column_dimensions["G"].width = 15
+        ws.column_dimensions["F"].width = 15
+        ws.column_dimensions["AC"].width = 15
